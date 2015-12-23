a35d1e800ce7043b4ebe35f2fa05a0c49f86c232
William Wolf <wwolf@shopkeep.com>
2015-12-23 15:24:12 -0500

    Main.js with react inside successfully linked to index.html

---
+++ dotify/app/components/Main.js
@@ -0,0 +1,7 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+  <h1>Hello, freaking world!</h1>,
+  document.getElementById('content')
+);
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    entry: "./dotify/app/components/Main.js",
+    output: {
+        filename: "dotify/public/bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            }
+        ]
+    }
+}

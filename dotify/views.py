3a20ff8b6d5c097f7d39ef719aa68a996a775771
William Wolf <wwolf@shopkeep.com>
2015-12-24 11:19:39 -0500

    change colors on main page

--- dotify/views.py
+++ dotify/views.py
@@ -7,20 +7,5 @@ from dotify import app, recommended_songs_path
 
 
 @app.route("/")
-def hello():
-    # TODO: write a thing that wraps all this into `RecommendedSongsRenderer`
-    rendered = render_component(
-        path=recommended_songs_path,
-        props={
-            'songs': [
-                {'artist': 'Dude', 'title': 'Hey Ma'},
-                {'artist': 'Kanye', 'title': 'Yes!'}
-            ]
-        },
-        to_static_markup=True
-    )
-    return render_template('index.html', rendered=rendered)
-
-@app.route("/form")
-def form():
+def main():
     return render_template('index.html')

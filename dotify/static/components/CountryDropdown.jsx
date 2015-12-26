c8f3cc90412d1c6bb4eb1d52c1e403eda31a7266
William Wolf <wwolf@shopkeep.com>
2015-12-26 17:11:42 -0500

    add a dropdown menu, sort of

--- dotify/static/components/CountryDropdown.jsx
+++ dotify/static/components/CountryDropdown.jsx
@@ -5,15 +5,15 @@ import React from 'react';
 
 var CountryDropdown = React.createClass({
     render: function() {
-        var countries = this.props.countries.map(function(country) {
-            return (
-                <option value={country.id}>{country.name}</option>
-            )
-        })
         return (
-            <select value="1">
-                {countries}
-            </select>
+            <div className="dropdown">
+              <button className="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example</button>
+              <ul className="dropdown-menu">
+                <li><a href="#">HTML</a></li>
+                <li><a href="#">CSS</a></li>
+                <li><a href="#">JavaScript</a></li>
+              </ul>
+            </div>
         );
     }
 });
--- dotify/views.py
+++ dotify/views.py
@@ -18,6 +18,16 @@ def test():
         {'id': 1, 'name': 'This'},
         {'id': 2, 'name': 'Hey'},
         {'id': 3, 'name': 'Colombia'},
+        {'id': 3, 'name': 'Colombia'},
+        {'id': 3, 'name': 'Colombia'},
+        {'id': 3, 'name': 'Colombia'},
+        {'id': 3, 'name': 'Colombia'},
+        {'id': 3, 'name': 'Colombia'},
+        {'id': 3, 'name': 'Colombia'},
+        {'id': 3, 'name': 'Colombia'},
+        {'id': 3, 'name': 'Colombia'},
+        {'id': 3, 'name': 'Colombia'},
+        {'id': 3, 'name': 'Colombia'},
     ]
     rendered = render_component(country_dropdown_path, props={'countries': countries})
     return render_template('index.html', rendered=rendered)

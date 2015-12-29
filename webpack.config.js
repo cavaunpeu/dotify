249b0bc7c4f3e3e2f2497e1445e6f1deec915853
William Wolf <wwolf@shopkeep.com>
2015-12-28 23:07:26 -0500

    hello world with webpack-dev-server

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    context: __dirname + '/dotify/static/js',
+    entry: './main.js',
+    output: {
+        path: __dirname + '/dotify',
+        publicPath: '/assets/',
+        filename: 'index.js'
+    },
+    devServer: {
+        inline: true,
+        port: 3333,
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+}

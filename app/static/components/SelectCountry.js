b33b21c42cb184fc670636bd461c2f99e9aafd4e
William Wolf <wwolf@shopkeep.com>
2016-01-03 22:30:23 -0500

    render country menu below input form

--- app/static/components/SelectCountry.js
+++ app/static/components/SelectCountry.js
@@ -1,12 +1,28 @@
 import React from 'react';
 
 var SelectCountry = React.createClass({
+  renderMenu: function() {
+    var countryMenu = function() {
+      return (
+        <ul>
+          <li>"hey"</li>
+          <li>"dude"</li>
+        </ul>
+      );
+    };
+    return (
+      <div className="country-menu">
+        {countryMenu()}
+      </div>
+    );
+  },
   render: function() {
     return (
       <div className="select-country">
         <form>
           <input type="text" />
         </form>
+        {this.renderMenu()}
       </div>
     );
   }

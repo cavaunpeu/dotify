e382f9d8b2971aef4a95e5ae4f80c169d428e261
William Wolf <wwolf@shopkeep.com>
2016-01-09 17:50:14 -0600

    only display countries in dropdown that contain current input string

--- dotify/static/components/country/Dropdown.js
+++ dotify/static/components/country/Dropdown.js
@@ -1,18 +1,33 @@
 import React from 'react';
 
 var Dropdown = React.createClass({
+  componentWillMount: function () {
+    this.setState({
+      countries: [
+        {id: 1, name: "Colombia"},
+        {id: 2, name: "Puerto Rico"},
+        {id: 3, name: "Mexico"},
+        {id: 4, name: "Venezuela"},
+        {id: 5, name: "Chile"},
+        {id: 6, name: "Cuba"},
+        {id: 7, name: "Guatemala"},
+        {id: 8, name: "Brazil"},
+      ]
+    });
+  },
   render: function() {
-    var menu = function() {
-      return (
-        <ul>
-          <li>"pa mexico"</li>
-          <li>"tal vez"</li>
-        </ul>
-      );
-    }
+    var countriesToList = this.state.countries.map(function(country) {
+      if (country.name.includes(this.props.inputValue)) {
+        return (
+          <li key={country.id}>{country.name}</li>
+        );
+      }
+    }.bind(this));
     return (
       <div className="country-menu">
-        {menu()}
+        <ul>
+          {countriesToList}
+        </ul>
       </div>
     );
   }

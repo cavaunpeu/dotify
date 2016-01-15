46e561ccfd401476820065d90ff2d54c6311b11b
William Wolf <wwolf@shopkeep.com>
2016-01-14 19:08:19 -0600

    add <input> that expands with user text * remove border-box * put jQuery code inline, which we will remove

--- dotify/static/components/select/Input.js
+++ dotify/static/components/select/Input.js
@@ -1,14 +1,46 @@
-import React from 'react';
+import React from "react";
 
 var Input = React.createClass({
   propTypes: {
     handleInputChange: React.PropTypes.func.isRequired,
     placeholder: React.PropTypes.string.isRequired
   },
+  componentDidMount: function() {
+    function expandInputElement(text) {
+      if (!text.trim()) {
+          text = $(this).attr("placeholder").trim();
+      }
+      var $span = $(this).parent().find("span");
+      $span.text(text);
+      $(this).css("width", $span.width());
+    }
+
+    // initialize text element
+    $(".Input").find("input").each(function () {
+      expandInputElement.call($(this), $(this).val())
+    });
+
+    // handle text change
+    $(".Input").find("input").keypress(function (e) {
+      if (e.which && e.charCode) {
+        var c = String.fromCharCode(e.keyCode | e.charCode);
+        expandInputElement.call($(this), $(this).val() + c);
+      }
+    });
+
+    // handle backspaces
+    $(".Input").find("input").keyup(function (e) { 
+      if (e.keyCode === 8 || e.keyCode === 46) {
+        expandInputElement.call($(this), $(this).val());
+      }
+    });
+
+  },
   render: function() {
     return (
       <div className="Input">
         <input placeholder={this.props.placeholder} onChange={this.props.handleInputChange}/>
+        <span></span>
       </div>
     )
   }

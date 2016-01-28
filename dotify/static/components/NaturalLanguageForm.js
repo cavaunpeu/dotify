9e043fbdbc0bd20dce70a38b607c432ff664e3ac
William Wolf <wwolf@shopkeep.com>
2016-01-27 19:30:06 -0600

    set enteredDropdownElementName on state, to be passed to backend

--- dotify/static/components/NaturalLanguageForm.js
+++ dotify/static/components/NaturalLanguageForm.js
@@ -6,32 +6,38 @@ var NaturalLanguageForm = React.createClass({
   getInitialState: function () {
     return {
       componentsToRender: [
-        <CountrySelect flexOrder={1} handleValidInput={this.handleValidInput}/>
+        {
+          "component": <CountrySelect flexOrder={1} handleValidInput={this.handleValidInput}/>,
+          "enteredDropdownElementName": ""
+        }
       ]
     }
   },
   determineNextComponent: function (flexOrder) {
-    return this.isEven(flexOrder) ?
+    var nextComponent = this.isEven(flexOrder) ?
         <CountrySelect flexOrder={flexOrder + 1} handleValidInput={this.handleValidInput}/>
-      :<OperatorSelect flexOrder={flexOrder + 1} handleValidInput={this.handleValidInput}/>
+      :<OperatorSelect flexOrder={flexOrder + 1} handleValidInput={this.handleValidInput}/>;
+    return {
+      "component": nextComponent,
+      "enteredDropdownElementName": ""
+    };
   },
   handleValidInput: function (flexOrder, inputValue) {
     if (flexOrder == this.state.componentsToRender.length) {
+      this.setState((state) => { componentsToRender: state.componentsToRender[flexOrder - 1]["enteredDropdownElementName"] = inputValue })
       if (inputValue != "=") {
-        this.setState({
-          componentsToRender: this.state.componentsToRender.concat([this.determineNextComponent(flexOrder)])
-        });
+        this.setState((state) => { componentsToRender: state.componentsToRender.push(this.determineNextComponent(flexOrder)) })
       }
     }
   },
   isEven: function(integer) {
-    return integer % 2 == 0
+    return integer % 2 == 0;
   },
   render: function () {
-    var componentsToRender = this.state.componentsToRender.map(function(component) {
+    var componentsToRender = this.state.componentsToRender.map(function(componentObject) {
       return (
-        <span className="nl-form-component" key={component.props.flexOrder}>
-          {component}
+        <span className="nl-form-component" key={componentObject["component"].props.flexOrder}>
+          {componentObject["component"]}
         </span>
       );
     });

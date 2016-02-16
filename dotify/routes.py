df9c056d06fbe2162992181b5881af2b6e42e0dc
William Wolf <wwolf@shopkeep.com>
2016-02-16 14:21:08 -0600

    remove hard-coded countries in OperatorSelect and load via ajax in componentDidMount

--- dotify/routes.py
+++ dotify/routes.py
@@ -4,7 +4,7 @@ from flask import jsonify, request, render_template
 
 from dotify import app
 from dotify.database import session
-from dotify.models import Country
+from dotify.models import Country, Operator
 
 
 @app.route('/', methods=['GET'])
@@ -16,6 +16,12 @@ def main():
 def get_countries():
     countries = [{'id': country.id, 'name': country.name, 'value': country.value} for country in session.query(Country)]
     return jsonify({'countries': countries}), 200
+
+
+@app.route('/operators', methods=['GET'])
+def get_operators():
+    operators = [{'id': operator.id, 'name': operator.name, 'value': operator.value} for operator in session.query(Operator)]
+    return jsonify({'operators': operators}), 200
 
 
 @app.route('/songs', methods=['POST'])
--- dotify/static/components/select/operator/Select.js
+++ dotify/static/components/select/operator/Select.js
@@ -2,25 +2,34 @@ import React from 'react'
 import Select from '../../Select'
 import Operator from './Operator'
 
+var $ = require('jquery');
+
 var OperatorSelect = React.createClass({
 
   placeholder: "+",
+  source: "/operators",
 
-  fetchDropdownElements: function () {
-    return (
-      [
-        <Operator id={0} name="&#43;"   value="+" />,
-        <Operator id={1} name="&#8722;" value="-" />,
-        <Operator id={2} name="&#215;"  value="*" />,
-        <Operator id={3} name="&#247;"  value="/" />,
-        <Operator id={4} name="&#61;"   value="=" />,
-      ]
-    );
+  getInitialState: function () {
+    return {
+      dropdownElements: []
+    };
+  },
+  componentDidMount: function() {
+    this.serverRequest = $.get(this.source, function (response) {
+      this.setState({
+        dropdownElements: response['operators'].map(function(operator) {
+          return <Operator id={operator.id} name={operator.name} value={operator.value}/>;
+        })
+      });
+    }.bind(this));
+  },
+  componentWillUnmount: function() {
+    this.serverRequest.abort();
   },
   render: function () {
     return (
       <div className="operator-select">
-        <Select dropdownElements={this.fetchDropdownElements()} flexOrder={this.props.flexOrder} handleValidDropdownElement={this.props.handleValidDropdownElement} placeholder={this.placeholder}/>
+        <Select dropdownElements={this.state.dropdownElements} flexOrder={this.props.flexOrder} handleValidDropdownElement={this.props.handleValidDropdownElement} placeholder={this.placeholder}/>
       </div>
     );
   }

f6991b0caaa66f996b2f90846131d2b0bb526f45
William Wolf <wwolf@shopkeep.com>
2016-03-18 11:43:24 -0400

    change from 4-space indent to 2-space indent

--- webpack.config.js
+++ webpack.config.js
@@ -5,47 +5,47 @@ var ManifestRevisionPlugin = require('manifest-revision-webpack-plugin');
 var rootAssetPath = './dotify/static'
 
 module.exports = {
-    entry: {
-        main_js: [
-            rootAssetPath + '/js/main.js'
-        ],
-        info_js: [
-            rootAssetPath + '/js/info.js'
-        ]
-    },
-    output: {
-        path: './build/public',
-        publicPath: 'http://localhost:3333/assets/',
-        filename: '[name].[chunkhash].js',
-        chunkFilename: '[id].[chunkhash].js'
-    },
-    devServer: {
-        inline: true,
-        port: 3333,
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel',
-                query: {
-                    presets: ['es2015', 'react']
-                }
-            },
-            {
-                test: /\.css$/,
-                loader: 'style!css'
-            },
-            {
-                test: /\.less$/,
-                loader: "style!css!less"
-            },
-        ]
-    },
-    plugins: [
-        new ManifestRevisionPlugin(path.join('build', 'manifest.json'), {
-            rootAssetPath: rootAssetPath
-        })
+  entry: {
+    main_js: [
+      rootAssetPath + '/js/main.js'
+    ],
+    info_js: [
+      rootAssetPath + '/js/info.js'
     ]
+  },
+  output: {
+    path: './build/public',
+    publicPath: 'http://localhost:3333/assets/',
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[id].[chunkhash].js'
+  },
+  devServer: {
+    inline: true,
+    port: 3333,
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+         }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css'
+      },
+      {
+        test: /\.less$/,
+        loader: "style!css!less"
+      },
+    ]
+  },
+  plugins: [
+    new ManifestRevisionPlugin(path.join('build', 'manifest.json'), {
+      rootAssetPath: rootAssetPath
+    })
+  ]
 }

43c1e35be964989cd69474390f6d2408fa006deb
William Wolf <wwolf@shopkeep.com>
2016-12-27 16:36:59 +0000

    compute predicted preferences via @ operator and abstract into separate method

--- dotify/recommendation/song_generator.py
+++ dotify/recommendation/song_generator.py
@@ -14,6 +14,7 @@ class SongGenerator:
         self.operator_objects = operator_objects
         self.country_vectors = country_vectors
         self.song_vectors = song_vectors
+        self.song_vectors_index = pd.DataFrame(song_vectors).index
 
     def _compute_aggregate_vector(self):
         aggregate_vector = self.country_vectors.pop(0)
@@ -21,9 +22,12 @@ class SongGenerator:
             aggregate_vector = OPERATORS[operator.id]['function'](aggregate_vector, vector)
         return aggregate_vector
 
-    def __iter__(self):
+    def _compute_predicted_preferences(self):
         aggregate_vector = self._compute_aggregate_vector()
-        predicted_preferences = pd.DataFrame(self.song_vectors).apply(lambda vec: np.dot(vec, aggregate_vector), axis=1)
+        return pd.Series( np.array(self.song_vectors) @ np.array(aggregate_vector), index=self.song_vectors_index )
+
+    def __iter__(self):
+        predicted_preferences = self._compute_predicted_preferences()
         for song_id, vector in predicted_preferences.sort_values(ascending=False).head(self.NUM_SONGS_TO_RECOMMEND).iteritems():
             song = session.query(Song).filter(Song.id == int(song_id)).first()
             yield (song.title, song.artist, song.url)
